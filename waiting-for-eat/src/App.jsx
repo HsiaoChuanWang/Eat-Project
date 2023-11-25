5a190c11849603683f284cd4fd1f680f018982f9
HsiaoChuanWang <j2130970@gmail.com>
2023-11-25 11:50:04 +0800

    Change position of react-router setting from App.jsx into main.jsx.

--- waiting-for-eat/src/App.jsx
+++ waiting-for-eat/src/App.jsx
@@ -1,26 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Calendar from "./pages/Calendar";
-import Homepage from "./pages/Homepage";
-import Login from "./pages/Login";
-import Post from "./pages/Post";
-import Editor from "./pages/Post/TextEditor";
-import Search from "./pages/Search";
-import Test from "./pages/test";
-
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/editor" element={<Editor />} />
-          <Route path="/post" element={<Post />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/test" element={<Test />} />
-        </Routes>
-      </BrowserRouter>
+      {/* <Header /> */}
+      <Outlet />
+      {/* <Footer /> */}
     </>
   );
 }
--- waiting-for-eat/src/main.jsx
+++ waiting-for-eat/src/main.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+import Calendar from "./pages/Calendar";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Post from "./pages/Post";
+import Editor from "./pages/Post/TextEditor";
+import Search from "./pages/Search";
+import Test from "./pages/test";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes path="/" element={<App />}>
+        <Route index element={<Homepage />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/editor" element={<Editor />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
