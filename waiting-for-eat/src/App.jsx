acc273c8eb7388ac046b8b6d68028654e29cd6b4
HsiaoChuanWang <j2130970@gmail.com>
2023-12-25 16:50:39 +0800

    【Debug in App component】 - Block user from accessing the reservation page, diner page and boss page without logging in. - Block logged in user from accessing other users' diner pages or boss pages.

--- waiting-for-eat/src/App.jsx
+++ waiting-for-eat/src/App.jsx
@@ -1,7 +1,7 @@
 import { ConfigProvider } from "antd";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import useHeaderStore from "../src/stores/headerStore";
 import useUserStore from "../src/stores/userStore.js";
 import Footer from "./components/Footer";
@@ -21,28 +21,48 @@ function App() {
   );
   const setHeader = useHeaderStore((state) => state.setHeader);
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
+    const pathParts = location.pathname.split("/");
+    const firstParam = pathParts[1];
+    const thirdParam = pathParts[3];
+    console.log(thirdParam);
+
     onAuthStateChanged(auth, (user) => {
+      if (user === null) {
+        setHeader("LogOut");
+        const logOutCannotEnterPath = ["reserve", "diner", "boss"];
+        if (logOutCannotEnterPath.some((path) => firstParam?.includes(path))) {
+          navigate("/");
+        }
+      }
+
       if (user) {
         const user = auth.currentUser;
+        const userId = user.uid;
+        console.log(userId);
+
         Promise.all([
-          setUserId(user.uid),
-          getUserInfoFromFirestoreAndSave(user.uid),
+          setUserId(userId),
+          getUserInfoFromFirestoreAndSave(userId),
         ]).then(([_, userInfo]) => {
-          if (userInfo.userName !== "" && userInfo.companyId === "") {
+          if (userInfo.companyId === "") {
             setHeader("DinerLogIn");
-          } else if (userInfo.userName !== "" && userInfo.companyId !== "") {
-            navigate(`/boss/bossInfo/${userInfo.companyId}`);
+
+            firstParam?.includes("boss") ||
+              (!thirdParam?.includes(userId) && navigate("/"));
+          } else if (userInfo.companyId !== "") {
             setHeader("BossLogIn");
             getCompanyInfoFromFirestoreAndSave(userInfo.companyId);
-          } else {
-            setHeader("LogOut");
+
+            !thirdParam?.includes(userInfo.companyId) &&
+              navigate(`/boss/bossInfo/${userInfo.companyId}`);
           }
         });
       }
     });
-  }, [isLogin]);
+  }, [location.pathname]);
 
   return (
     <ConfigProvider
